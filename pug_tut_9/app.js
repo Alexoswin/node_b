06814b574d58d1617ecf67209af8dc0dafc86ffa
oswin <oswinalex1@gmail.com>
2023-08-16 00:05:42 +0530

    pug with plane html

---
+++ pug_tut_9/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 80;
+
+// Serving static files
+app.use('/static', express.static('static'));
+
+// Setting the template engine as pug
+app.set('view engine', 'pug');
+
+// Setting the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Route handling
+app.get('/', (req, res) => {
+    res.status(200).render('index'); // connecting 'index.pug' file in the 'views' folder
+});
+
+// Starting the server
+app.listen(port, () => {
+    console.log("Application is running at local server port 80");
+});
