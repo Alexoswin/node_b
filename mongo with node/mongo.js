7a70c0a045b08ea4a44f2f51d1ea4728d7b72df5
oswin <oswinalex1@gmail.com>
2023-09-29 09:34:43 +0530

    mongo

--- mongo with node/mongo.js
+++ mongo with node/mongo.js
@@ -1,32 +1,43 @@
-const mongoose = require("mongoose");
 
-// Define an async function for your main logic
-async function main() {
-  try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/employee');
+      const mongoose = require("mongoose");
 
-    // Define your schema and model
-    const kittySchema = new mongoose.Schema({
-      name: String
-    });
+      // Define an async function for your main logic
+      async function main() {
+          try {
+              await mongoose.connect('mongodb://127.0.0.1:27017/employee');
 
-    const Kitten = mongoose.model('Kitten', kittySchema);
+              // Define your schema and model
+              const custSchema = new mongoose.Schema({
+                  name: String,
+                  email: String,
+                  password: String
+              });
 
-    // Create a new kitten document
-    const fluffy = new Kitten({ name: 'Fluffy' });
+              const cust = mongoose.model('cust', custSchema);
 
-    // Save the document to the database
-    await fluffy.save();
-    
-    console.log('Kitten saved successfully.');
-  } catch (err) {
-    console.error('Error:', err);
-  } finally {
-    // Close the Mongoose connection when you're done
-    mongoose.connection.close();
-  }
-}
+              // Create a customer collection and add customer details
+              const custdetails = new cust({ name: "oswin", email: "siasfk", password: "cpassword" });
+              const custdetails1 = new cust({ name: "cname", email: "cemail", password: "cpassword" });
+              const custdetails2 = new cust({ name: "fscname", email: "sfcemail", password: "cpassword" });
 
-main().catch(err => console.log(err));
+              
 
+              
+              // Save the customer data to the database
+              await custdetails.save();
+              await custdetails1.save();
+              await custdetails2.save();
+              
+              console.log('saved in datatbase')
+
+          } catch (err) {
+              console.error('Error:', err);
+          } finally {
+              // Close the Mongoose connection when you're done
+              mongoose.connection.close();
+          }
+      }
+
+      main().catch(err => console.log(err));
+      console.log('closing mongo connection')
 
